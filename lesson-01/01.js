62c7332828985c1c9bda79b8e074128b14984f20
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-07 21:47:50 +0300

    first commit

---
+++ lesson-01/01.js
@@ -0,0 +1,10 @@
+// С помощью цикла while найдите сумму чисел от 1 до 20
+// Результат присвойте переменной sum
+
+// your code
+let meaning = 1;
+let sum = 0;
+while (meaning <= 20) {   
+    sum += meaning;
+    meaning++; 
+};
