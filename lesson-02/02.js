7aa5b9ba366727ec0e8470dae4854fa26cd2c9a8
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-13 21:54:13 +0300

    1

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,14 +7,17 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 6) + 1; // your code
+let dice1 = Math.floor(Math.random() * 6) + 1;// your code
 let dice2 = Math.floor(Math.random() * 6) + 1;// your code
-let isWinningDouble; // your code
-
-if(dice1===dice2){
-  isWinningDouble=true;
+let isWinningDouble // your code
+console.log('Первый бросок: ' + dice1);
+console.log('Второй бросок: ' + dice2);
+if (dice1 === dice2) {
+  console.log('Выигрышный дубль!')
+} else {
+  console.log('Не выигрышный дубль.')
 }
-
+/*
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
 if (isWinningDouble) {
@@ -22,4 +25,4 @@ if (isWinningDouble) {
 } else {
   console.log('Не выигрышный дубль.')
 }
-
+*/
