d0aba0dfe8b6e5528855eccb743250cc30e03e2b
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-13 21:48:30 +0300

    second task2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -10,12 +10,16 @@
 let dice1 = Math.floor(Math.random() * 6) + 1; // your code
 let dice2 = Math.floor(Math.random() * 6) + 1;// your code
 let isWinningDouble; // your code
-if (dice1 === dice2) {
-  isWinningDouble = true;
+
+if(dice1===dice2){
+  isWinningDouble=true;
 }
-console.log('Первый бросок: ' + dice1);
-console.log('Второй бросок: ' + dice2);
-console.log(isWinningDouble ? 'Выигрышный дубль!' : 'Не выигрышный дубль.')
 
-
+console.log('Первый бросок: ' + dice1)
+console.log('Второй бросок: ' + dice2)
+if (isWinningDouble) {
+  console.log('Выигрышный дубль!')
+} else {
+  console.log('Не выигрышный дубль.')
+}
 
