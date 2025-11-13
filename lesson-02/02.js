900a1b2ef1823f369d34baab3b244f60823f61ae
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-13 21:41:33 +0300

    2 task

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,8 +7,8 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 = Math.floor(Math.random() * 5) + 1; // your code
-let dice2 = Math.floor(Math.random() * 5) + 1;// your code
+let dice1 = Math.floor(Math.random() * 6) + 1; // your code
+let dice2 = Math.floor(Math.random() * 6) + 1;// your code
 let isWinningDouble; // your code
 if (dice1 === dice2) {
   isWinningDouble = true;
