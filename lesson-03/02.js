cfca3ca74b9a00cfbd27adc6233d0fa76c40e60e
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-20 11:59:00 +0300

    hello js 3 lesson

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,10 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(meaning) {
+    if (meaning % 2 === 1) {
+        return false;
+    } else {
+        return true;
+    }
+}
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,5 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(one, two, three) {
+    return Math.max(one, two, three);
+}
