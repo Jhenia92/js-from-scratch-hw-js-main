45cec10965686aded76417b78ac78c12787b34cb
E.I.Arabina <E.I.Arabina92@gmail.com>
2025-11-20 12:03:50 +0300

    hello3 lesson

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -2,9 +2,12 @@
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
 function isEven(meaning) {
+    meaning=Math.abs(meaning);
     if (meaning % 2 === 1) {
         return false;
     } else {
         return true;
     }
 }
+
+console.log(isEven(-1))
